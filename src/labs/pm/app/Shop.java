4e98424bb66b7b1fa55ca925255676aef46c7046
MihaiTrandafir <trandamaster@gmail.com>
2025-04-11 10:47:52 +0300

    lambda expressions

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -14,11 +14,13 @@ package labs.pm.app;
   @author oracle
  */
 
+import labs.pm.data.Product;
 import labs.pm.data.ProductManager;
 import labs.pm.data.Rating;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Comparator;
 
 /**
  @author MihaiTrandafir
@@ -28,30 +30,41 @@ public class Shop {
     public static void main(String[] args) {
         ProductManager pm = new ProductManager("en-GB");
         pm.createProduct(101,"Tea",BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-        pm.printProductReport(101);
+//        pm.printProductReport(101);
         pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea");
         pm.reviewProduct(101, Rating.TWO_STAR, "Rather weak tea");
         pm.reviewProduct(101, Rating.FOUR_STAR, "Fine tea");
         pm.reviewProduct(101, Rating.FOUR_STAR, "Good tea");
         pm.reviewProduct(101, Rating.FIVE_STAR, "Perfect tea");
         pm.reviewProduct(101, Rating.THREE_STAR, "Just add some lemon");
-        pm.printProductReport(101);
-        pm.changeLocale("ru-RU");
+//        pm.printProductReport(101);
+//        pm.changeLocale("ru-RU");
         pm.createProduct(102,"Coffee",BigDecimal.valueOf(1.99), Rating.NOT_RATED);
         pm.reviewProduct(102, Rating.THREE_STAR, "Coffee was ok");
         pm.reviewProduct(102, Rating.ONE_STAR, "Where is the milk?!");
         pm.reviewProduct(102, Rating.FIVE_STAR, "It's perfect with the tea spoon of sugar!");
-        pm.printProductReport(102);
+//        pm.printProductReport(102);
         pm.createProduct(103,"Cake",BigDecimal.valueOf(3.99), Rating.NOT_RATED, LocalDate.now().plusDays(2));
         pm.reviewProduct(103, Rating.FIVE_STAR, "Very nice cake");
         pm.reviewProduct(103, Rating.FOUR_STAR, "Good, but I've expected more chocolate");
         pm.reviewProduct(103, Rating.FIVE_STAR, "This cake is perfect!");
-        pm.printProductReport(103);
-        pm.changeLocale("fr-FR");
+//        pm.printProductReport(103);
+//        pm.changeLocale("fr-FR");
         pm.createProduct(104,"Cookie",BigDecimal.valueOf(2.99), Rating.NOT_RATED, LocalDate.now());
         pm.reviewProduct(104, Rating.THREE_STAR, "Just another cookie");
         pm.reviewProduct(104, Rating.THREE_STAR, "OK");
-        pm.printProductReport(104);
-
+//        pm.printProductReport(104);
+        Comparator<Product> ratingSorter = (p1, p2) ->
+                p2.getRating().ordinal() - p1.getRating().ordinal();
+        Comparator<Product> priceSorter = (p1, p2) ->
+                p2.getPrice().compareTo(p1.getPrice());
+        pm.printProducts(ratingSorter.thenComparing(priceSorter));
+        pm.printProducts(ratingSorter.thenComparing(priceSorter).reversed());
+        pm.printProducts((p1, p2) ->
+                p2.getRating().ordinal() - p1.getRating().ordinal());
+//        pm.printProducts((p1, p2) ->
+//                p1.getRating().ordinal() - p2.getRating().ordinal());
+        pm.printProducts((p1, p2) ->
+                p2.getPrice().compareTo(p1.getPrice()));
     }
 }
